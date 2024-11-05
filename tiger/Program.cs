8d96451ee8f447144693b30013fec731ecee3a0f
You Gao <djx3rn@virginia.edu>
2024-11-04 20:48:48 -0500

    chore: initial new proj

---
+++ tiger/Program.cs
@@ -0,0 +1,10 @@
+using tiger;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
