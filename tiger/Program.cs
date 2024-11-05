38403883b53524a2cf30a96bbe215820e4f48fa0
You Gao <djx3rn@virginia.edu>
2024-11-05 00:21:35 -0500

    feat: IT WORKS?

--- tiger/Program.cs
+++ tiger/Program.cs
@@ -5,33 +5,39 @@ using System.Net.Mail;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
-    .WriteTo.File("C:/ProgramData/YourServiceName/logs.txt", rollingInterval: RollingInterval.Day) // Add this line to write logs to a file
+    .WriteTo.File("C:/ProgramData/tiger-daemon/logs.txt", rollingInterval: RollingInterval.Day) // Add this line to write logs to a file
     .CreateLogger();
 
 try
 {
     Log.Information("Starting up");
+    Log.Information($"AppContext.BaseDirectory: {AppContext.BaseDirectory}");
 
-    SmtpClient smtpClient = new SmtpClient("mail.MyWebsiteDomainName.com", 25);
+    // Load configuration from config file
+    var configuration = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("C:/Users/commo/OneDrive - University of Virginia/School/STEM/CS/Coding Projects 2024/tiger-daemon/tiger/config", optional: false, reloadOnChange: true)
+    .Build();
 
-    smtpClient.Credentials = new System.Net.NetworkCredential("info@MyWebsiteDomainName.com", "myIDPassword");
-    // smtpClient.UseDefaultCredentials = true; // uncomment if you don't want to use the network credentials
+    IConfigurationSection smtpSettings = configuration.GetSection("SmtpSettings");
+    string smtpServer = smtpSettings["Server"];
+    int smtpPort = int.Parse(smtpSettings["Port"]);
+    string smtpEmail = smtpSettings["Email"];
+    string smtpPassword = smtpSettings["Password"];
+
+    // Set up SMTP client from config 
+    SmtpClient smtpClient = new SmtpClient(smtpServer, smtpPort);
+
+    smtpClient.Credentials = new System.Net.NetworkCredential(smtpEmail, smtpPassword);
     smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
     smtpClient.EnableSsl = true;
-    MailMessage mail = new MailMessage();
-
-    //Setting From , To and CC
-    mail.From = new MailAddress("info@MyWebsiteDomainName", "MyWeb Site");
-    mail.To.Add(new MailAddress("info@MyWebsiteDomainName"));
-    mail.CC.Add(new MailAddress("MyEmailID@gmail.com"));
-
-    smtpClient.Send(mail);
 
     IHost host = Host.CreateDefaultBuilder(args)
         .UseSerilog() // Add this line to use Serilog
         .UseWindowsService() // Add this line to integrate with Windows Service
         .ConfigureServices(services =>
         {
+            services.AddSingleton(smtpClient);
             services.AddHostedService<Worker>();
         })
         .Build();
--- tiger/Worker.cs
+++ tiger/Worker.cs
@@ -1,22 +1,59 @@
 using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Net.Mail;
+using Microsoft.Win32.SafeHandles;
 
 namespace tiger;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly SmtpClient _smtpClient;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, SmtpClient smtpClient)
     {
         _logger = logger;
+        _smtpClient = smtpClient;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        int count = 0;
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            bool isDiscordRunning = Process.GetProcessesByName("Discord").Any();
+            await Task.Delay(60000, stoppingToken);                
+             if (isDiscordRunning)
+            {
+                count++;
+                _logger.LogInformation($"Discord is running. Count: {count}");
+            }
+            else
+            {
+                _logger.LogWarning("Discord is not running.");
+            }            
+            if (count == 2) {
+                SendEmail("Discord is running", "Discord is running on your computer.", "djx3rn@virginia.edu");
+                count = 0;
+            }
+
+        }
+    }
+
+    public async void SendEmail(string subject, string body, string to)
+    {
+        try
+        {
+            var mailMessage = new MailMessage("henrygao00@gmail.com", to)
+            {
+                Subject = subject,
+                Body = body
+            };
+            await _smtpClient.SendMailAsync(mailMessage);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error sending email.");
         }
     }
 }
